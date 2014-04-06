ed70f3eb1e6100d3244cd25bc370fbd1ce62fb3d
Rafał Słota <roxeon@gmail.com>
2014-04-06 05:19:20 +0200

    Territory <-> Region relationship

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,5 +1,7 @@
 class Region < ActiveRecord::Base
   # Not null
-  validates :RegionID, presence: true
+  validates :RegionID, presence: true, uniqueness: true
   validates :RegionDescription, presence: true
+
+  has_many :territories, primary_key: "RegionID", foreign_key: "RegionID", dependent: :restrict
 end
--- app/models/territory.rb
+++ app/models/territory.rb
@@ -1,6 +1,10 @@
 class Territory < ActiveRecord::Base
   # Not null
-  validates :TerritoryID, presence: true
+  validates :TerritoryID, presence: true, uniqueness: true
   validates :TerritoryDescription, presence: true
   validates :RegionID, presence: true
+
+
+  belongs_to :region, class_name: "Region", foreign_key: "RegionID", inverse_of: :territories
+  validates :region, presence: true
 end
